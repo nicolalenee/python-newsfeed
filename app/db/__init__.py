db469c18df6d83076497d182829b7b788f717444
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-10-22 16:56:12 -0400

    add jinja filters to templates

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
@@ -10,3 +11,22 @@ load_dotenv()
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db(app):
+  Base.metadata.create_all(engine)
+  # Tell flask to run close_db() whenever a context is destroyed
+  app.teardown_appcontext(close_db)
+
+# return a new session-connection object
+def get_db():
+  if 'db' not in g:
+    # store db connection in app context
+    g.db = Session()
+  return g.db
+
+def close_db(e=None):
+  # find and remove db from the g object
+  db = g.pop('db', None)
+  # if db exists then close the connection
+  if db is not None:
+    db.close()
